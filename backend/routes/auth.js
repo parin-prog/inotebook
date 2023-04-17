37b2033e6550404e49c1d9643188690a9a73e60b
parin-prog <parinvaghani001@gmail.com>
2023-04-17 16:47:59 +0530

    Route 2: User login, Validation & JWT token added

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -25,16 +25,17 @@ body('password').isLength({ min: 5 })], async(req, res) => {
     }
 
     // Password encryption for security
-    const salt = bcrypt.genSalt(10);
-    const secPass = bcrypt.hash(req.body.password, salt);
+    const salt = await bcrypt.genSalt(10);
+    const secPass = await bcrypt.hash(req.body.password, salt);
 
     // Create a new user
-    user = User.create({
+    user = await User.create({
         name: req.body.name,
         email: req.body.email,
         password: secPass
-    }).then(user => { res.send(user) })
-        .catch(err => { console.log(err) })
+    })
+    // .then(user => { res.send(user) })
+    // .catch(err => { console.log(err) })
 
     // Adding JWT token for authentication purpose
     const data = {
@@ -44,8 +45,41 @@ body('password').isLength({ min: 5 })], async(req, res) => {
     }
 
     const authtoken = jwt.sign(data, JWT_SECRET);
-    res.json(authtoken);
+    res.json({authtoken});
 
+})
+
+// Route 2: Authenticate user using: POST "/api/auth/login". No login required
+router.post('/login', [body('email').isEmail({min:5},
+            body('password').exists(),
+            )], async (req, res) => {
+                // If there are errors, return Bad request and the errors.
+                const errors = validationResult(req);
+                if(!errors.isEmpty()) {
+                    return res.status(400).json({errors: errors.array()})
+                }
+
+                const {email, password} = req.body;
+
+                // find data from db by email
+                let user = await User.findOne({email});
+                if(!user) {
+                    return res.status(400).json({error: 'Please try to login with correct credentials'})
+                }
+                // decode the password & match with correct password
+                const passwordCompare = await bcrypt.compare(password, user.password);
+                if(!passwordCompare) {
+                    return res.status(400).json({error: 'Please try to login with correct password'})
+                }
+
+                // JWT token for authentication
+                const data = {
+                    user: {
+                      id: user.id
+                    }
+                  }
+                  const authtoken = jwt.sign(data ,JWT_SECRET);
+                  res.json(authtoken);
 })
 
 module.exports = router;
