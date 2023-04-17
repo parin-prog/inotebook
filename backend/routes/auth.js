e4b64b8e5a17da79c2b3fa792424fcd6d3900d06
parin-prog <parinvaghani001@gmail.com>
2023-04-17 12:27:00 +0530

    Route 1 :  Create a user with POST

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,9 +1,16 @@
 const express = require('express')
-// const User = require('../models/User')
+const User = require('../models/User')
 const router = express.Router();
 
+// Route 1: Create a user using : POST "/api/auth/createuser". No login required
 router.post('/createuser', (req, res) => {
-    res.send(req.body);
+    const user = User.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password
+    }).then(user => {res.send(user)})
+    .catch(err => {console.log(err)})
+
 })
 
 module.exports = router;
--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -2,6 +2,7 @@ const express = require('express')
 // const Note = require('../models/Note')
 const router = express.Router();
 
+// Route 1: Create a note using : POST "/api/notes/createnote". Login required
 router.post('/createnote', (req, res) => {
     res.send(req.body);
 })
